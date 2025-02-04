cdfa1010411a9f441e3d833a544937b3692b1bd0
Daniel Kopaczewski <kopaczewskidaniel@gmail.com>
2025-02-04 23:33:20 +0100

    Add component for displaying car, filters, page for displaying cars list, car controller

---
+++ CarMarket/Server/Controllers/CarController.cs
@@ -0,0 +1,30 @@
+﻿using CarMarket.Server.Data;
+using CarMarket.Shared;
+using CarMarket.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarMarket.Server.Controllers;
+
+[ApiController]
+[AllowAnonymous]
+[Route($"api/{Routes.CarApi}/[action]")]
+public class CarController(ILogger<ChatMessageController> logger) : ControllerBase
+{
+    private readonly ILogger<ChatMessageController> _logger = logger;
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Car>> Get()
+    {
+		try
+        {
+            var cars = ExampleCars.GetAllCars();
+            return Ok(cars);
+        }
+		catch (Exception ex)
+        {
+            _logger.LogError("Exception - \n\tType: {type}\n\tMessage: {message}", ex.GetType(), ex.Message);
+            return BadRequest(ex.Message);
+        }
+    }
+}
