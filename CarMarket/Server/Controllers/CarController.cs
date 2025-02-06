409fd3e0fbf4c38f8dd1d6f60d990e35891cb6a6
Daniel Kopaczewski <d.kopaczewski@gotec-group.com>
2025-02-06 12:37:30 +0100

    Task2 - solution

--- CarMarket/Server/Controllers/CarController.cs
+++ CarMarket/Server/Controllers/CarController.cs
@@ -12,4 +12,19 @@ namespace CarMarket.Server.Controllers;
 public class CarController(ILogger<ChatMessageController> logger) : ControllerBase
 {
     private readonly ILogger<ChatMessageController> _logger = logger;
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Car>> Get()
+    {
+		try
+        {
+            var cars = ExampleCars.GetAllCars();
+            return Ok(cars);
+        }
+		catch (Exception ex)
+        {
+            _logger.LogError("Exception - \n\tType: {type}\n\tMessage: {message}", ex.GetType(), ex.Message);
+            return BadRequest(ex.Message);
+        }
+    }
 }
